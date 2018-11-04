5b8dea4787913deb094ce8513245766db3eb6223
Gorbunov Pavel <pachagorbunoff@yandex.ru>
2018-11-04 21:41:37 +0300

    done server

--- task_queue/server.py
+++ task_queue/server.py
@@ -1,27 +1,27 @@
 
-import pdb
 import argparse
 import socket
+import time
 from collections import deque
 from hashlib import sha256
 
 class TaskQueueServer:
 
     def __init__(self, ip, port, path, timeout):
-        print(ip, "==>", port)
         self.path = path
-        self.timeout = timeout
-        self.queue = Queues()
+        self.queue = Queues(timeout)
         self.socket = socket.socket()
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind((ip, port))
-        self.socket.listen(2)
+        self.socket.listen(1)
 
     def run(self):
         while True:
+            self.queue.check_time()
             (conn, address) = self.socket.accept()
             handler = Handler(conn, address, self.queue)
             handler.handle()
+            
 
     def __del__(self):
         self.socket.close()
@@ -34,79 +34,118 @@ class Handler:
         self.queue = queue
     
     def handle(self):
-        data = self.socket.recv(1024).decode().strip()
-        print(data)
-        command, args = data.split(' ', 1)
-        response = self.proccess(command, args)
-        print("response = ", response)
-        print("End connection to {}".format(self.address))
+        data = self.socket.recv(10**6+1).decode().strip()
+        response = self.proccess(data)
         self.socket.send(response.encode())
         self.socket.close()
     
-    def proccess(self, act, args):
-        print(act)
+    def proccess(self, data):
+        data = data.split()
+        act = data.pop(0)
         commands = {
             'GET': self.queue.get,
             'ACK': self.queue.ack,
             'ADD': self.queue.add,
             'IN': self.queue.check_in,
-            'SAVE': self.queue.save
+            'SAVE': self.queue.save,
         }
         try:
-            response = commands[act](args.split())
+            response = commands[act](*data)
             return response
-        except Exception as e:
-            print(e)
+        except Exception:
             return "ERROR"
 
 
 class Queues:
-    def __init__(self):
+    def __init__(self, timeout):
         self.queues = {}
-        self.len = 0
+        self.que_process = {}
+        self.proc_count = 0
+        self.timeout = timeout
     
-    def get(self, args):
-        name = args
+    def get(self, name):
         if name not in self.queues or not len(self.queues[name]):
-            return None
+            return "NONE"
         task = self.queues[name].popleft()
+        task.prepare(self.proc_count)
+        self.que_process[name][task.id] = task
+        self.proc_count += 1
         return str(task)
     
-    def add(self, args):
-        print('args = ', args)
-        queue, length, data = args
+    def add(self, queue, length, data):
         task = Task(length, data)
         if queue not in self.queues:
             self.queues[queue] = deque()
+            self.que_process[queue] = dict()
         self.queues[queue].append(task)
         return task.id
 
     
-    def ack(self, args):
-        pass
+    def ack(self, name, task_id):
+        if task_id not in self.que_process[name].keys():
+            return "NO"
+        del self.que_process[name][task_id]
+        self.update_proc(name)
+        return "YES"
+        
+    def update_proc(self, name):
+        self.proc_count = 0
+        new_proc = dict()
+        for key, value in sorted(self.que_process[name].items(), key=lambda elem: elem[1].pos):
+            value.pos = self.proc_count
+            new_proc[key] = value
+            self.proc_count += 1
+        self.que_process[name] = new_proc
+        
     
-    def check_in(self, args):
-        pass
+    def check_in(self, name, task_id):
+        if not name in self.queues:
+            return "NO"
+        if task_id in [elem.id for elem in self.queues[name]] \
+            or \
+            task_id in self.que_process[name].keys():
+            return "YES"
+        return "NO"
     
-    def save(self, args):
-        pass
+    def check_time(self):
+        delete = []
+        for ques in self.que_process.keys():
+            if not len(self.que_process[ques]):
+                continue
+            for key, task in sorted(self.que_process[ques].items(), key= lambda elem: elem[1].pos):
+                if int(time.time() - task.time) >= self.timeout:
+                    delete.append((ques,key))
+        if not delete:
+            return
+        for elem in delete:
+            que, key = elem
+            task = self.que_process[que].pop(key)
+            self.queues[que].insert(int(task.pos), task)
+
+    def save(self):
+        return "OK"
 
 
 class Task:
     def __init__(self, length, data):
         self.length = length
         self.data = data
-        self.status = 0
+        self.time = 0
         self.id = self.get_id()
+        self.pos = -1
     
     def get_id(self):
         global number
         number += 1
-        return sha256(str(number).encode()).hexdigest()
+        return str(number)
+    
+    def prepare(self, pos):
+        self.pos = pos
+        self.time = time.time()
     
     def __str__(self):
         return "{} {} {}".format(self.id, self.length, self.data)
-    
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='This is a simple task queue server with custom protocol')
@@ -115,7 +154,7 @@ def parse_args():
         action="store",
         dest="port",
         type=int,
-        default=8080,
+        default=5555,
         help='Server port')
     parser.add_argument(
         '-i',
@@ -141,9 +180,8 @@ def parse_args():
     return parser.parse_args()
 
 
-number = 0
-
 if __name__ == '__main__':
+    number = 0
     args = parse_args()
     server = TaskQueueServer(**args.__dict__)
     server.run()
